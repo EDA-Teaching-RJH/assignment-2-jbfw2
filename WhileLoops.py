f6d9664c724eaca289b6feca187aa33928afc818
jbfw2 <jbfw2@kent.ac.uk>
2024-10-24 18:35:44 +0000

    numbers

--- WhileLoops.py
+++ WhileLoops.py
@@ -1,1 +1,12 @@
 ### Part Two -- your code goes here. 
+import random
+number = random.randint(1,100)
+guess = None
+while guess != number:
+    guess = int(input("Guess a number between 1 and 100!"))
+    if guess < number:
+        print("Too low. guess again")
+    elif guess > number:
+        print("Too high. Guess again")    
+    else:
+        print(f"Well done. {number} is the correct answer")
--- forLoop.py
+++ forLoop.py
@@ -1,1 +1,5 @@
 ### Part One -- your code goes here. 
+
+for i in range (1,11):
+    square = i ** 2
+    print (f"The square of {i} is {square}")
